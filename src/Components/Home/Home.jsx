ee3c87b966fd0b1868fb0cc57f39ea08ecc383da
hiculbc <praveen.baira@beautifulcode.in>
2024-01-05 23:37:55 +0530

    feat: App started

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Header } from "../Common/Header";
+
+export const Home = () => {
+    return (
+        <React.Fragment>
+            <Header />
+        </React.Fragment>
+    )
+}
